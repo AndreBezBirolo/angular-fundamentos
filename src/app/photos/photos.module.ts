8284ac7098eb914e0006ea7dbf9519258dbbd9c3
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-13 15:41:40 -0300

    Implementado pipe para filtrar galeria e ngIf

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,19 +4,21 @@ import {CommonModule} from '@angular/common';
 
 import {PhotoComponent} from './photo/photo.component';
 import {PhotoListComponent} from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
+import {PhotoFormComponent} from './photo-form/photo-form.component';
+import {PhotosComponent} from './photo-list/photos/photos.component';
+import {FilterByDescriptionPipe} from './photo-list/filter-by-description.pipe';
 
 @NgModule({
   declarations: [
     PhotoComponent,
     PhotoListComponent,
     PhotoFormComponent,
-    PhotosComponent
+    PhotosComponent,
+    FilterByDescriptionPipe
   ],
   imports: [
     HttpClientModule,
-    CommonModule
+    CommonModule,
   ]
 })
 export class PhotosModule {
