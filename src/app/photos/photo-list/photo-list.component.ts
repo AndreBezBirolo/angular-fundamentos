ee08f43755757c2c9ee6f97e855ef94d69c24327
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-14 10:49:29 -0300

    Debounce não resolvido

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,24 +1,31 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Subject} from 'rxjs';
+
 
 import {Photo} from '../photo/photo';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.photos = this.activatedRoute.snapshot.data.photos
+    this.photos = this.activatedRoute.snapshot.data.photos;
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
 
   onKeyUp(target : any) {
@@ -26,6 +33,10 @@ export class PhotoListComponent implements OnInit {
       let element = target as HTMLInputElement;
       this.filter = element.value;
     }
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
 }
