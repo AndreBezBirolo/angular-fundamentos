36f51576f5a966c1e2dca4f344e9c0ab2e284174
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-14 14:05:45 -0300

    Criação de submódulos para organização

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -5,6 +5,7 @@ import {Subject} from 'rxjs';
 
 import {Photo} from '../photo/photo';
 import {debounce, debounceTime} from 'rxjs/operators';
+import {PhotoService} from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -16,12 +17,17 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
   constructor(
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
   ) {}
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data.photos;
     this.debounce
       .pipe(debounceTime(300))
@@ -33,6 +39,15 @@ export class PhotoListComponent implements OnInit, OnDestroy {
       let element = target as HTMLInputElement;
       this.debounce.next(element.value)
     }
+  }
+
+  load() {
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) this.hasMore = false
+      })
   }
 
   ngOnDestroy(): void {
