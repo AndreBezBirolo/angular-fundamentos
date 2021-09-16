bc646069e43d2efca5aaab3408ee96b16bb5aa52
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-16 10:13:50 -0300

    Criado o componente do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import {PhotosModule} from './photos/photos.module';
 import {AppRoutingModule} from './app.routing.module';
 import {ErrorsModule} from './errors/errors.module';
 import {HomeModule} from './home/home.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
     ErrorsModule,
-    HomeModule
+    HomeModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
