a3f7a3bf3eac3154e349bba2b9b7532e773f0315
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-17 11:34:27 -0300

    criado o rodapé

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,31 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header/header.component';
 import {RouterModule} from '@angular/router';
+import {providerDef} from '@angular/compiler/src/view_compiler/provider_compiler';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {RequestInterceptor} from './auth/request.interceptor';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent],
-  exports: [HeaderComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
     RouterModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestInterceptor,
+      multi: true
+    }
   ]
 })
 export class CoreModule {
