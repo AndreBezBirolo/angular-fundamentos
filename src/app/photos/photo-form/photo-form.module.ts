2b8a8c8ae795a5a086642f227570045411329c65
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-17 14:51:23 -0300

    Ao abrir formulário de nova foto, fazer o input abrir automáticamente

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -5,6 +5,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {VmessageModule} from '../../shared/components/vmessage/vmessage.module';
 import {RouterModule} from '@angular/router';
 import {PhotoModule} from '../photo/photo.module';
+import {ImmediateClickModule} from '../../shared/directives/immediate-click/immediate-click.module';
 
 @NgModule({
   imports: [
@@ -13,7 +14,8 @@ import {PhotoModule} from '../photo/photo.module';
     VmessageModule,
     FormsModule,
     RouterModule,
-    PhotoModule
+    PhotoModule,
+    ImmediateClickModule
   ],
   declarations: [
     PhotoFormComponent,
