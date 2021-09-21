3263e4a0787a5198e0865ece9aa24862c870556e
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-21 08:58:07 -0300

    Implementado tratamento de erro global com envio para api de log

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://suaapientraaqui.com.br'
+  API_URL: 'https://suaapientraaqui.com.br',
+  LOG_API_URL: 'https://suaapiaqui.com.br'
 };
