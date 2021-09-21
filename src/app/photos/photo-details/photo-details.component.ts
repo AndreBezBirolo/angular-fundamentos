19b83a49543a4176418520e15586bfcb65b6b4a5
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-09-21 09:27:44 -0300

    Removendo history api do browser

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -38,7 +38,7 @@ export class PhotoDetailsComponent implements OnInit {
     this.photoService.removePhoto(this.photoId)
       .subscribe(() => {
           this.alertService.success('Photo removed!', true);
-          this.router.navigate(['/user', this.userService.getUserName()]);
+          this.router.navigate(['/user', this.userService.getUserName()], {replaceUrl: true});
         },
         error => this.alertService.warning('Could not delete the photo!', true))
   }
